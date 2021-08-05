3cfd362677f3ab7a65d9dbaf4bf0cf5e890e1e14
MauroAlbrecht <mauroint@gmail.com>
2021-08-05 20:08:23 -0300

    first commit

---
+++ src/main/java/repository/QueriesExecution.java
@@ -0,0 +1,49 @@
+package repository;
+
+import model.Aluno;
+
+import java.util.List;
+
+public class QueriesExecution {
+
+    public static void main(String[] args) {
+
+        AlunoDAO alunoDAO = new AlunoDAO();
+
+        // =========================== 1 - Consulta =================================================
+        List<Aluno> alunos = alunoDAO.list();
+
+        alunos.stream().forEach(System.out::println);
+
+
+        // ======================= 1.1 - Consulta com filtro ========================================
+        Aluno alunoParaConsulta = alunoDAO.getById(1);
+
+        //System.out.println(alunoParaConsulta);
+
+
+        // =========================== 2 - Inserção =================================================
+        Aluno alunoParaInsercao = new Aluno(
+                "Mauro",
+                30,
+                "PR"
+        );
+
+        //alunoDAO.create(alunoParaInsercao);
+
+
+        // =========================== 3 - Delete ===================================================
+        //alunoDAO.delete(1);
+
+
+        // =========================== 4 - Atualizar ================================================
+        Aluno alunoParaAtualizar = alunoDAO.getById(3);
+        alunoParaAtualizar.setNome("Debora");
+        alunoParaAtualizar.setIdade(26);
+        alunoParaAtualizar.setEstado("PR");
+
+        //alunoDAO.update(alunoParaAtualizar);
+    }
+
+}
+
